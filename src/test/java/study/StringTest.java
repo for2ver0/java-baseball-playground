e8d5bfabeab04d19192d8d6e514a7839ddbd5997
eun0 <shinely12@gmail.com>
2025-08-01 15:48:05 +0900

    test: String 클래스에 대한 학습 테스트(1)
    1. "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인
    2. "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지 확인

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,36 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @DisplayName("[요구사항1] 두 개의 문자가 ','로 split 됩니다")
+    @Test
+    void two_characters_are_split_with_comma() {
+        String actual = "1,2";
+
+        String[] result = actual.split(",");
+        assertThat(result).contains("1");
+        assertThat(result).contains("2");
+        assertThat(result).containsExactly("1", "2");
+
+        printActualResult(result);
+    }
+
+    @DisplayName("[요구사항1] 한 개의 문자를 ','로 split한 경우, 해당 문자가 그대로 나온다")
+    @Test
+    void the_character_will_be_displayed_as_is_if_split_a_single_character_with_comma() {
+        String actual = "1";
+
+        String[] result = actual.split(",");
+        assertThat(result).containsExactly("1");
+
+        printActualResult(result);
+    }
+
+    private void printActualResult(String[] actual) {
+        System.out.println("[Actual Results]");
+        for (String s : actual) {
+            System.out.println(s);
+        }
     }
 }
