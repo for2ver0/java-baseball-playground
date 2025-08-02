460a94a9dd00a3c6cd39a12e2305835f207d13f7
eun0 <shinely12@gmail.com>
2025-08-03 01:24:56 +0900

    feat: StringCalculator
    - 사용자가 입력한 문자열 값에 따라 사칙연산을 수행할 수 있는 계산기
    - 문자열 계산기는 사칙연산의 계산 우선순위가 아닌 입력 값에 따라 계산 순서가 결정
      - 예를 들어, "2 + 3 * 4 / 2"와 같은 문자열을 입력할 경우 2 + 3 * 4 / 2 실행 결과인 10을 출력

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class StringCalculator {
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        double result = stringCalculate(userInput);
+        printResult(result);
+    }
+
+    private double stringCalculate(String scannerInput) {
+        String[] inputs = parseInput(scannerInput);
+
+        return calculate(inputs);
+    }
+
+    private String[] parseInput(String input) {
+        return input.split(" ");
+    }
+
+    private double calculate(String[] inputs) {
+        double operand1 = Integer.parseInt(inputs[0]);
+        for (int i = 1; i < inputs.length; i += 2) {
+            String operator = inputs[i];
+            double operand2 = Integer.parseInt(inputs[i + 1]);
+
+            operand1 = customCalculator(operand1, operand2, operator);
+        }
+
+        return operand1;
+    }
+
+    private double customCalculator(double operand1, double operand2, String operator) {
+        switch (operator) {
+            case "+":
+                operand1 += operand2;
+                break;
+            case "-":
+                operand1 -= operand2;
+                break;
+            case "*":
+                operand1 *= operand2;
+                break;
+            case "/":
+                operand1 /= operand2;
+                break;
+            default:
+                throw new StringIndexOutOfBoundsException();
+        }
+        return operand1;
+    }
+
+    private void printResult(double result) {
+        System.out.println(result);
+    }
+}
