6a2d1f3f05291c58df099dd36b19f5fda2206fe3
eun0 <shinely12@gmail.com>
2025-08-03 15:48:10 +0900

    feat: 컴퓨터 숫자 생성 (Green)
    - 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 선택한다

--- src/main/java/NumberBaseballGame.java
+++ src/main/java/NumberBaseballGame.java
@@ -1,2 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumberBaseballGame {
+
+    public int[] generateRandomNumber() {
+        // 1부터 9사이의 서로 다른 3개의 난수
+        int min = 1;
+        int max = 9;
+        int count = 3;
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        while (uniqueNumbers.size() < count) {
+            int randomNumber = (int)(Math.random() * (max - min + 1) + min);
+            uniqueNumbers.add(randomNumber);
+        }
+
+        return uniqueNumbers.stream().mapToInt(Integer::intValue).toArray();
+    }
+
 }
