2f982de2450320243f7a9e1c448a3cffc27aa936
eun0 <shinely12@gmail.com>
2025-08-04 01:27:26 +0900

    feat(ConsoleOutputHandler-showGameWinMessage): 두 번째 출력 메소드 (Green)
    - 컴퓨터가 선택한 3개의 숫자를 모두 맞히면, 게임에서 승리한다
    - 게임에서 승리하면 해당 게임이 종료된다
    - 플레이어에게 승리 메시지와 게임 종료 메시지를 출력한다

--- src/main/java/io/ConsoleOutputHandler.java
+++ src/main/java/io/ConsoleOutputHandler.java
@@ -6,4 +6,8 @@ public class ConsoleOutputHandler {
         System.out.print("숫자를 입력해 주세요 : ");
     }
 
+    public void showGameWinMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
 }
